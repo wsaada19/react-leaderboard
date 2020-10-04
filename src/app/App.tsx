a274cef75f3f358cbe99c79652853b852cf2d6a0
wsaada19 <william.saada@richmond.edu>
2020-10-03 21:05:28 -0400

    cleaning up and reformating

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Leaderboard from './components/Leaderboard';
-import './scss/index.scss';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Leaderboard from "./components/Leaderboard";
+import "./scss/index.scss";
 
-ReactDOM.render(<Leaderboard className="wrapper" 
-                    title= {"Kills Leaderboard"}
-                    subtitle = {"Do your best to avoid these players..."} 
-                    headings = {["Name", "Score"]}
-                    rows = {[["Will", 1005], ["Bill", 934], ["Bob", 456]]}
-                     />,
-        document.getElementById('root'));
+ReactDOM.render(
+  <Leaderboard
+    className="wrapper"
+    title={"Most Kills"}
+    subtitle={"Do your best to avoid these players..."}
+    headings={["Name", "Score"]}
+    rows={[
+      ["Will", 1005],
+      ["Bill", 934],
+      ["Bob", 456],
+    ]}
+  />,
+  document.getElementById("root")
+);
